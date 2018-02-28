0091a4b55a307c3c84dd1d37a2f54e4b9d5ae9ed
Maik Macho <nuda1998@gmail.com>
2018-02-28 18:39:06 +0100

    Update

--- styles/docfx.js
+++ styles/docfx.js
@@ -531,7 +531,7 @@ $(function () {
   function renderAffix() {
     var hierarchy = getHierarchy();
     if (hierarchy.length > 0) {
-      var html = '<h5 class="title">In This Article</h5>'
+      var html = '<h5 class="title">Contents</h5>'
       html += util.formList(hierarchy, ['nav', 'bs-docs-sidenav']);
       $("#affix").empty().append(html);
       if ($('footer').is(':visible')) {
