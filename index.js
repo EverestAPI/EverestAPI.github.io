cc1b415d40bad438bca3f93c620d4e022e030d47
Maddie <52103563+maddie480@users.noreply.github.com>
2024-11-06 14:46:56 +0100

    Update macOS install instructions with "enhanced" unverified apps blocking

--- index.js
+++ index.js
@@ -74,3 +74,32 @@
         }
     }
 })();
+
+{
+    // handling for the macOS instructions foldable section
+    const fold = document.getElementById("mac-instructions-fold");
+    const icon = document.getElementById("mac-instructions-fold-icon");
+    let folded = false;
+
+    const toggle = () => {
+        if (folded) {
+            // unfold
+            icon.innerText = "-";
+            fold.style.display = "block";
+            folded = true;
+        } else {
+            // fold
+            icon.innerText = "+";
+            fold.style.display = "none";
+            folded = false;
+        }
+    };
+
+    document.getElementById("mac-instructions-fold-trigger").addEventListener("click", e => {
+        e.preventDefault();
+        toggle();
+    });
+
+    // the section is unfolded by default in the HTML, but we want it folded by default if JS is enabled
+    toggle();
+}
