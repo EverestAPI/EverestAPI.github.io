1092ea5e4ca34c1c04d47e339a578692fdf3ffd0
Maddie <52103563+maddie480@users.noreply.github.com>
2025-12-14 00:33:44 +0100

    Use mirrored versions lists instead of maddie480.ovh on the home page
    Some people can't access maddie480.ovh... and since they're on everestapi.github.io, might as well fetch the versions lists from the same website.

--- index.js
+++ index.js
@@ -12,33 +12,26 @@
     // === Fetch link to Everest
 
     {
-        // a static file on this website indicates to Everest and Olympus where the Everest versions list is provided,
-        // so load the URL from there as well...
-        const updaterUrlFetch = await fetch("/everestupdater.txt");
+        // call the API mirror on this very website to retrieve the versions list
+        const versionListFetch = await fetch("https://everestapi.github.io/updatermirror/everest_versions.json");
 
-        if (updaterUrlFetch.ok) {
-            // ... then call it.
-            const updaterUrl = await updaterUrlFetch.text();
-            const versionListFetch = await fetch(updaterUrl.trim() + "?supportsNativeBuilds=true");
+        if (versionListFetch.ok) {
+            const versionList = await versionListFetch.json();
 
-            if (versionListFetch.ok) {
-                const versionList = await versionListFetch.json();
+            const stable = getLinkForBranch(versionList, "stable").mainDownload;
+            const beta = getLinkForBranch(versionList, "beta").mainDownload;
+            const dev = getLinkForBranch(versionList, "dev").mainDownload;
 
-                const stable = getLinkForBranch(versionList, "stable").mainDownload;
-                const beta = getLinkForBranch(versionList, "beta").mainDownload;
-                const dev = getLinkForBranch(versionList, "dev").mainDownload;
+            // if all versions have an existing build...
+            if (stable !== null && beta !== null && dev !== null) {
+                // set the links to their artifacts
+                document.getElementById("latest-stable-link").href = stable;
+                document.getElementById("latest-beta-link").href = beta;
+                document.getElementById("latest-dev-link").href = dev;
 
-                // if all versions have an existing build...
-                if (stable !== null && beta !== null && dev !== null) {
-                    // set the links to their artifacts
-                    document.getElementById("latest-stable-link").href = stable;
-                    document.getElementById("latest-beta-link").href = beta;
-                    document.getElementById("latest-dev-link").href = dev;
-
-                    // remove the line saying "Click the '1 published' button under 'Related', then 'main' to download it." since those are now direct links.
-                    var artifactInstructions = document.getElementById("artifact-instructions");
-                    artifactInstructions.parentNode.removeChild(artifactInstructions);
-                }
+                // remove the line saying "Click the '1 published' button under 'Related', then 'main' to download it." since those are now direct links.
+                var artifactInstructions = document.getElementById("artifact-instructions");
+                artifactInstructions.parentNode.removeChild(artifactInstructions);
             }
         }
     }
@@ -47,29 +40,24 @@
     // === Fetch link to Olympus
 
     {
-        const updaterUrlFetch = await fetch("/olympusupdater.txt");
+        // call the API mirror on this very website to retrieve the versions list
+        const versionListFetch = await fetch("https://everestapi.github.io/updatermirror/olympus_versions.json");
 
-        if (updaterUrlFetch.ok) {
-            // ... then call it.
-            const updaterUrl = await updaterUrlFetch.text();
-            const versionListFetch = await fetch(updaterUrl.trim());
+        if (versionListFetch.ok) {
+            const versionList = await versionListFetch.json();
 
-            if (versionListFetch.ok) {
-                const versionList = await versionListFetch.json();
+            const stable = getLinkForBranch(versionList, "stable");
 
-                const stable = getLinkForBranch(versionList, "stable");
+            if (stable !== null) {
+                // set the links to the latest stable
+                document.getElementById("olympus-macos-latest-link").href = stable.macosDownload;
+                document.getElementById("olympus-linux-latest-link").href = stable.linuxDownload;
 
-                if (stable !== null) {
-                    // set the links to the latest stable
-                    document.getElementById("olympus-macos-latest-link").href = stable.macosDownload;
-                    document.getElementById("olympus-linux-latest-link").href = stable.linuxDownload;
-
-                    // remove the line saying "Click the '5 published' button under 'Related', then '...main' to download it." since those are now direct links.
-                    const artifactInstructionsMacOS = document.getElementById("olympus-macos-artifact-instructions");
-                    artifactInstructionsMacOS.parentNode.removeChild(artifactInstructionsMacOS);
-                    const artifactInstructionsLinux = document.getElementById("olympus-linux-artifact-instructions");
-                    artifactInstructionsLinux.parentNode.removeChild(artifactInstructionsLinux);
-                }
+                // remove the line saying "Click the '5 published' button under 'Related', then '...main' to download it." since those are now direct links.
+                const artifactInstructionsMacOS = document.getElementById("olympus-macos-artifact-instructions");
+                artifactInstructionsMacOS.parentNode.removeChild(artifactInstructionsMacOS);
+                const artifactInstructionsLinux = document.getElementById("olympus-linux-artifact-instructions");
+                artifactInstructionsLinux.parentNode.removeChild(artifactInstructionsLinux);
             }
         }
     }
